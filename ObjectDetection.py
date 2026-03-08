da2e346d55c4ee90a24641265913f952d7c080f2
samuelhsuu <hsusamuel01@gmail.com>
2026-03-08 01:04:16 -0500

    Updated FPS counter

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -1,30 +1,33 @@
 import cv2
 from ultralytics import YOLO
 import time
+from collections import deque
 
-#load model weights (small)
+# Load model weights (small)
 model = YOLO("yolov8s.pt")
-#open camera, can also use filepath for video
+# Open camera, can also use filepath for video
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
   print("Error: could not open camera")
   exit()
 
-#objects with confidence <0.4 are filtered out
+# Objects with confidence <0.4 are filtered out
 CONFIDENCE_THRESHOLD = 0.4
 prev_time = time.time()
 
+# Running inference on every other frame for optimization
 SKIP_FRAMES = 2
 frame_count = 0
-last_results = [] # previous frame detections
+last_results = [] # revious frame detections
+
+fps_history = deque(maxlen=30) # Double ended queue of the last 30 frames
 
 while True:
-  #frame is a 3d array containing image data
+  # Frame is a 3d array containing image data
   ret, frame = cap.read()
 
   frame_count+=1
-
   if frame_count % SKIP_FRAMES == 0:
     # run inference on every other frame
     last_results = model(frame, conf=CONFIDENCE_THRESHOLD, imgsz = 320, verbose=False)
@@ -32,11 +35,12 @@ while True:
   if not ret:
     print("Error: failed to read frame")
     break
-  #model is called like a function to analyze the current frame, specify imgsz for optimization
+  # model is called like a function to analyze the current frame, specify imgsz for optimization
   # results = model(frame, conf=CONFIDENCE_THRESHOLD, imgsz = 320, verbose=False)
 
   #results only really has one item (the current frame)
   for result in last_results:
+
     #each box has all detections >0.4 confidence
     for box in result.boxes:
       x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -53,9 +57,11 @@ while True:
       cv2.putText(frame, text, (x1+3, y1-4), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
 
   curr_time = time.time()
-  fps = 1.0/(curr_time-prev_time)
+  fps_history.append(curr_time - prev_time)
   prev_time = curr_time
-  cv2.putText(frame, f"FPS: {fps:.1f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (50, 200, 255), 2)
+
+  avg_fps = 1.0/(sum(fps_history) / len(fps_history)) # Average the frames
+  cv2.putText(frame, f"FPS: {avg_fps:.1f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (50, 200, 255), 2)
 
   cv2.imshow("Object Detection", frame)
 
