3ade6ae41b4e1c70b83a3f92c7f9528ceb4bfaf4
samuelhsuu <hsusamuel01@gmail.com>
2026-03-14 21:21:46 -0400

    Added README, train.py supports graphics card

---
+++ README.md
@@ -0,0 +1,6 @@
+## Model weights
+Download best.pt from https://drive.google.com/drive/folders/1S_mwacJ4vLF9NjUG0x0wFLeforl24lq1?usp=drive_link
+
+Or retrain from scratch:
+1. Dataset from Roboflow (https://universe.roboflow.com/catwithawand/hand-detection-fuao9)
+2. Run python train.py
--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-model.train(
-  data = "dataset/data.yaml",
-  epochs = 50,
-  imgsz = 640,
-  batch = 8,
-  name="hand_detector"
-)
+if __name__ == '__main__':    
+  model = YOLO("yolov8n.pt")
+  model.train(
+    data = "dataset/data.yaml",
+    device = 0,
+    workers = 2,
+    epochs = 50,
+    imgsz = 640,
+    batch = 8,
+    name="hand_detector"
+  )
